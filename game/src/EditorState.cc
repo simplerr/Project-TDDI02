438600c389565580e26875560f51c4520bf2de86
ockomock <dralzki@hotmail.com>
2014-11-11 19:54:03 +0100

    EditorState added

---
+++ game/src/EditorState.cc
@@ -0,0 +1,31 @@
+#include "EditorState.h"
+
+EditorState::EditorState()
+{
+	mMenu = nullptr;
+}
+
+void EditorState::init()
+{
+	;
+}
+
+void EditorState::cleanup()
+{
+	;
+}
+
+void EditorState::update(float dt)
+{
+	;
+}
+
+void EditorState::draw(Renderer* renderer)
+{
+	;
+}
+
+void EditorState::handleEvent(SDL_Event e)
+{
+	;
+}
---
+++ game/src/EditorState.h
@@ -0,0 +1,24 @@
+#ifndef EDITROSTATE_H
+#define EDITORSTATE_H
+
+#include "BaseState.h"
+#include "Renderer.h"
+#include "Texture.h"
+
+class EditorState : public BaseState {
+ public:
+	EditorState();
+	~EditorState();
+
+	void init();
+	void cleanup();
+	void update(float dt);
+	void draw(Renderer* renderer);
+    void handleEvent(SDL_Event e);
+ private:
+	Texture *mMenu;
+};
+
+#endif
+
+
