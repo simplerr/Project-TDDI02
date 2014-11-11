bdcf55728152a3cc286629de34aeb62aa1b92ac3
simplerr <axel.blackert@gmail.com>
2014-11-11 12:51:54 +0100

    Added a file for declaring constants

---
+++ game/src/constants.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+static const std::string MENU_BKGD = "../imgs/menu_bkgd.png";
+static const std::string GRASS_PLATFORM = "../imgs/grass_platform.png";
+static const std::string ROCK_PLATFORM = "../imgs/rock_platform.png";
