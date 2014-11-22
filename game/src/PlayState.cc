58d4be3821ef288fa9dfa9479125c4c951a0cf4b
simplerr <axel.blackert@gmail.com>
2014-11-22 15:56:17 +0100

    Fixed level not restarting when falling outside map

--- game/src/PlayState.cc
+++ game/src/PlayState.cc
@@ -55,7 +55,7 @@ void PlayState::update(float dt)
 			setNextState(BaseState::MENU_STATE);
 		} 
 		else if (mPlayer->getPosition().y + mPlayer->getHeight() > mLevel->getLevelSize().y) // Trillar spelaren ned, ladda om banan
-			init(); // (Kan behövas ändras)
+		    init(mLevel->getCurrentLevel()); // (Kan behövas ändras)
 	}
 }
 
