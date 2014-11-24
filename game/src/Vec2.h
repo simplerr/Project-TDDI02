f6cc1bcd96465c8ab759b76cca2a9d30a1c016d1
AlexD <ernfridsson@gmail.com>
2014-11-24 18:35:47 +0100

    Lag till width och height i vec2

--- game/src/Vec2.h
+++ game/src/Vec2.h
@@ -6,16 +6,18 @@ struct Vec2
 {
     Vec2()
     {
-	x = y = 0;
+	x = y = w = h = 0;
     }
 
-    Vec2(int _x, int _y)
+    Vec2(int _x, int _y, int _w = 0, int _h = 0)
     {
 	x = _x;
 	y = _y;
+	w = _w;
+	h = _h;
     }
 
-    int x, y;
+    int x, y, w, h;
 };
 
 #endif
